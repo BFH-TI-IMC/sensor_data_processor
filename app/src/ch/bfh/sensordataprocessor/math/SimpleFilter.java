63fba13c7a7df72e3ae1ea61a88931e680564683
Patrick Hofer <patrick.hofer@bfh.ch>
2013-10-15 10:44:19 +0200

    Made a independent.

--- app/src/ch/bfh/sensordataprocessor/math/SimpleFilter.java
+++ app/src/ch/bfh/sensordataprocessor/math/SimpleFilter.java
@@ -18,14 +18,16 @@ package ch.bfh.sensordataprocessor.math;
 
 public class SimpleFilter {
 
-	static final float a = 0.1f;
+	static final float LOW_PASS_A = 0.1f;
+
+	static final float HIGH_PASS_A = 0.7f;
 
 	static float lowPass(float current, float last) {
-		return last * (1.0f - a) + current * a;
+		return last * (1.0f - LOW_PASS_A) + current * LOW_PASS_A;
 	}
 
 	static float highPass(float current, float last, float filtered) {
-		return a * (filtered + current - last);
+		return HIGH_PASS_A * (filtered + current - last);
 	}
 
 }
